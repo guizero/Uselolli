956b70a013ef1433a5bc3c4d2d38c587f65fa065
Guilherme Marques Peçanha Araujo <marques@gmail.com>
2013-05-09 00:27:01 -0300

    tentando resolver os problemas

--- catalog/language/portuguese/checkout/cart.php
+++ catalog/language/portuguese/checkout/cart.php
@@ -1,26 +1,26 @@
-﻿<?php
+<?php
 // Heading  
-$_['heading_title']          = 'Minha Sacola de Compras';
+$_['heading_title']          = 'Meu Carrinho de Compras';
 
 // Text
-$_['text_success']           = 'Você adicionou <a href="%s">%s</a> em sua <a href="%s">sacola de compras</a> com sucesso!';
-$_['text_remove']            = 'Você modificou sua sacola de compras com sucesso!';
+$_['text_success']           = 'Você adicionou <a href="%s">%s</a> em seu <a href="%s">carrinho de compras</a> com sucesso!';
+$_['text_remove']            = 'Você modificou seu carrinho de compras com sucesso!';
 $_['text_coupon']            = 'Você aplicou o cupom de desconto com sucesso!';
 $_['text_voucher']           = 'Você aplicou o vale-presente com sucesso!';
 $_['text_reward']            = 'Você aplicou os pontos de fidelidade com sucesso!';
 $_['text_shipping']          = 'O valor da forma de entrega selecionada foi aplicado com sucesso!';
 $_['text_login']             = 'Atenção: Você deve <a href="%s">entrar</a> ou <a href="%s">criar uma conta</a> para visualizar os preços!';
 $_['text_points']            = 'Pontos de fidelidade: %s';
-$_['text_items']             = 'Sacola: %s item(s) - %s';
+$_['text_items']             = 'Carrinho: %s item(s) - %s';
 $_['text_next']              = 'O que você gostaria de fazer em seguida?';
 $_['text_next_choice']       = 'Escolha se você tem um código de desconto ou pontos de fidelidade, ou se gostaria de calcular o custo do envio.';
 $_['text_use_coupon']        = 'Usar código de cupom';
 $_['text_use_voucher']       = 'Usar vale-presente';
 $_['text_use_reward']        = 'Usar pontos de fidelidade (%s disponíveis)';
 $_['text_shipping_estimate'] = 'Calcular Frete';
-$_['text_shipping_detail']   = 'Coloque seu CEP abaixo para ver quanto ficará o frete de sua sacola de compras.';
+$_['text_shipping_detail']   = 'Coloque seu CEP abaixo para ver quanto ficará o frete de seu carrinho.';
 $_['text_shipping_method']   = 'Selecione a forma de entrega desejada clicando em uma das opções abaixo.<br><br>';
-$_['text_empty']             = 'Sua sacola de compras está vazia!';
+$_['text_empty']             = 'Seu carrinho de compras está vazio!';
 
 
 // Column
@@ -45,7 +45,7 @@ $_['entry_postcode']         = 'CEP:';
 $_['error_stock']            = 'Produtos marcados com três asteriscos (***) não estão disponíveis na quantidade desejada, ou estão fora de estoque!';
 $_['error_minimum']          = 'Quantidade mínima para %s é %s!';	
 $_['error_required']         = '%s obrigatório!';	
-$_['error_product']          = 'Aviso: Não há produtos em sua sacola!';	
+$_['error_product']          = 'Aviso: Não há produtos em seu carrinho!';	
 $_['error_coupon']           = 'Aviso: Cupom é inválido, expirou ou atingiu seu limite de uso!';
 $_['error_voucher']          = 'Aviso: Vale-presente é inválido ou já foi usado!';
 $_['error_reward']           = 'Aviso: Por favor, coloque a quantidade de pontos de fidelidade que deseja usar!';	
