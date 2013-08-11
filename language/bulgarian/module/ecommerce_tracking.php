1fa04cca60fea890e5149336d344a2e712c4711f
Guilherme Marques Peçanha Araujo <marques@gmail.com>
2013-08-11 10:46:35 -0300

    adiciona ecommerce tracking

---
+++ language/bulgarian/module/ecommerce_tracking.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Google Analytics Ecommerce Tracking module for Opencart by Extensa Web Development
+ *
+ * Copyright © 2013 Extensa Web Development Ltd. All Rights Reserved.
+ * This file may not be redistributed in whole or significant part.
+ * This copyright notice MUST APPEAR in all copies of the script!
+ *
+ * @author 		Extensa Web Development Ltd. (www.extensadev.com)
+ * @copyright	Copyright (c) 2013, Extensa Web Development Ltd.
+ * @package 	Google Analytics Ecommerce Tracking module
+ * @link		http://www.opencart.com/index.php?route=extension/extension/info&extension_id=11242
+ */
+
+// Заглавие
+$_['heading_title']       = 'Ecommerce Tracking';
+
+// Текст
+$_['text_module']         = 'Допълнителни модули';
+$_['text_success']        = 'Готово, промените са запазени!';
+$_['text_product_id']     = 'Номер на продукта (ID)';
+$_['text_sku']            = 'Артикулен номер (SKU)';
+
+// Поле
+$_['entry_sku']           = 'Поле за Артикулен номер (SKU):';
+$_['entry_status']        = 'Статус:';
+
+// Грешка
+$_['error_permission']    = 'Внимание: Нямате права за промяна в секцията!';
+?>
